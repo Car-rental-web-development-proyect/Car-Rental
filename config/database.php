6597aaf72708f5ba18ca1abc369e8f9b26a15487
AntonioC003 <ma.castilloquintanilla@ugto.mx>
2024-06-04 01:01:30 -0600

    pwd

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 function connDB() : mysqli {
-    $conn = new mysqli('localhost','root','spartan1177','beecar');
+    $conn = new mysqli('localhost','root','mk4ncast','beecar');
     if(!$conn){
         echo "Error: no se logro conectar DB";
         exit;
